1c857ebb770da1f7ec1653411d8d2038c9550b77
porta8080 <adm.porta8080@gmail.com>
2016-12-05 02:03:55 -0200

    fix

--- resources/hello_world/hello_world_controller.js
+++ resources/hello_world/hello_world_controller.js
@@ -1,22 +1,10 @@
-var dao = require('./hello_world_dao');
 var bluebird = require('bluebird');
 
 function HelloWorldController(){
-  this.dao = bluebird.promisifyAll(dao);
 }
 
 HelloWorldController.prototype.index = function(req,res,next){
-  var controller = this;
-  this.dao.createAsync()
-  .then(function(){
-    return controller.dao.findAllAsync();
-  })
-  .then(function(data){
-    res.json(data);
-  })
-  .catch(function(err){
-    next(err);
-  });
+  res.send('aeho');
 };
 
 module.exports = new HelloWorldController();
--- resources/hello_world/hello_world_routes.js
+++ resources/hello_world/hello_world_routes.js
@@ -2,8 +2,6 @@ var express = require('express');
 var db = require('../../db');
 var router = express.Router();
 
-var model = require('./hello_world_model');
-var dao = require('./hello_world_dao');
 var controller = require('./hello_world_controller');
 
 router.get('/',controller.index.bind(controller));
