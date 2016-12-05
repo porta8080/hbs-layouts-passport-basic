0c7556bd0069f2b006cf283876ed9cd0e0d450cc
porta8080 <adm.porta8080@gmail.com>
2016-12-05 02:05:58 -0200

    fix

--- resources/hello_world/hello_world_routes.js
+++ resources/hello_world/hello_world_routes.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var db = require('../../db');
+//var db = require('../../db');
 var router = express.Router();
 
 var controller = require('./hello_world_controller');
